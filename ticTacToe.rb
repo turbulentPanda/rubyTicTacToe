1f7ff426d3caa78ca8a983a0301eb8c79dfd3b03
Christopher Marchant <cmarch240@gmail.com>
2021-12-16 12:57:21 -0600

    Created Game class and added instance methods update_board and display_board

--- ticTacToe.rb
+++ ticTacToe.rb
@@ -7,6 +7,34 @@ class Person
   end
 end
 
+class Game
+  attr_accessor :board
+
+  def initialize
+    @board = "
+      + - - + - - + - - +
+      |  1  |  2  |  3  |
+      + - - + - - + - - +
+      |  4  |  5  |  6  |
+      + - - + - - + - - +
+      |  7  |  8  |  9  |
+      + - - + - - + - - +
+    "
+  end
+
+  def update_board(position, marker)
+    @board.sub!(position.to_s, marker)
+  end
+
+  def display_board
+    puts @board
+  end
+end
+
+game1 = Game.new
+game1.display_board
+game1.update_board(5, "X")
+game1.display_board
 
 
 
