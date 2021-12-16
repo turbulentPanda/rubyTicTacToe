ec486d1acefe9bf7138a9e9a83719a232a7c3471
Christopher Marchant <cmarch240@gmail.com>
2021-12-16 13:34:26 -0600

    Added update_winning_positions method to Game class

--- ticTacToe.rb
+++ ticTacToe.rb
@@ -8,7 +8,7 @@ class Person
 end
 
 class Game
-  attr_accessor :board
+  attr_accessor :board, :winning_positions
 
   def initialize
     @board = "
@@ -20,21 +20,44 @@ class Game
       |  7  |  8  |  9  |
       + - - + - - + - - +
     "
+
+    @winning_positions = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7]
+    ]
+  end
+
+  def update_winning_positions(position, marker)
+    winning_positions.each do |row|
+      row.each_with_index do |column, column_index|
+        row[column_index] = marker if column == position
+      end
+    end
   end
 
   def update_board(position, marker)
-    @board.sub!(position.to_s, marker)
+    board.sub!(position.to_s, marker)
   end
 
   def display_board
-    puts @board
+    puts board
   end
 end
 
 game1 = Game.new
-game1.display_board
-game1.update_board(5, "X")
-game1.display_board
+# game1.display_board
+game1.update_winning_positions(1, "X")
+game1.update_winning_positions(2, "X")
+game1.update_winning_positions(3, "X")
+p game1.winning_positions
+# game1.update_board(5, "X")
+# game1.display_board
 
 
 
